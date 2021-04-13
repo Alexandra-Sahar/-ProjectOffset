402facb9be65c9c1f3ad9a0659404eb861ff86c0
Alexandra-Sahar <77438599+Alexandra-Sahar@users.noreply.github.com>
2021-04-13 12:22:20 +0900

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { render } from 'react-dom';
-import React from 'react';
-import App from './App';
-import "bootstrap/dist/css/bootstrap.min.css"; 
-/*подключение бутстрапа*/ 
-
-render(<App />, document.getElementById('root'));
--- src/pages/Contacts.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Contacts = () => (
-    <h1>Контакты (страница для проверки React-Router)</h1>
-)
-
-export default Contacts;
